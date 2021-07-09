9e2aa87779e07532686de8e0a9ca7fcff4b3e25c
joealmond <jozsef.mandula@gmail.com>
2021-07-09 22:21:12 +0200

    International news for you feature added.

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,26 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-// Want to use async/await? Add the `async` keyword to your outer function/method.
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=hu&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
   } catch (error) {
     console.error(error);
   }
+}
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+module.exports = async function () {
+  let newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((newsObjects) => {
+    console.log("newsObjects:", newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
